97bb213fb79058ba837d8753ae6d0b9f7c29be72
ELDRON64 <rethernet@gmail.com>
2026-07-22 15:48:54 +0200

    Fixed text and CMakelists

--- include/extensions/graphik/components/text.h
+++ include/extensions/graphik/components/text.h
@@ -4,16 +4,9 @@
 #include "camera.h"
 #include "../manager.hpp"
 
-#define HORIZONTAL_LEFT 0x1
-#define HORIZONTAL_CENTER 0x2
-#define HORIZONTAL_RIGTH 0x4
-#define VERTICAL_LEFT 0x10
-#define VERTICAL_CENTER 0x20
-#define VERTICAL_RIGTH 0x40
-
 class Text : public Behaviour {
 public:
-	enum ALIGNMENT {
+	enum class ALIGNMENT {
 		RIGHT,
 		CENTER,
 		LEFT,
@@ -26,26 +19,21 @@ private:
 	unsigned int VBO;
 	
 	std::string _text = "";
-	std::string _new_text = "";
-	ALIGNMENT _text_align_h = CENTER;
-	ALIGNMENT _text_align_v = CENTER;
+	ALIGNMENT _text_align_h = ALIGNMENT::CENTER;
+	ALIGNMENT _text_align_v = ALIGNMENT::CENTER;
 	bool _x_warp = false;
 	int instances = 0;
 
 	vec4 _color = {1,1,1,1};
-	bool is_up_to_date = false;
+	bool _is_up_to_date = false;
 
 	std::string _camera = "";
 	std::string _shader = ""; 
 	std::string _font = "";
 
 public:
-	void Update_Instance_Buffer ( ) {
-		// only one update for the buffer per frame
-		if ( is_up_to_date ) { return; }
-		is_up_to_date = true;
-		
-        DEBUG ( 5, "Updating Instance buffer Text");
+	void Update_Instance_Buffer ( ) {		
+		DEBUG ( 5, "Updating Instance buffer Text");
 
 		if ( _font == "" ) { return; }
 		auto font = Manager::Get < Font > ( _font );
@@ -57,13 +45,38 @@ public:
 		std::vector < int > comulative_x;
 		comulative_x.push_back ( 0 );
 		int new_lines = 0;
-		if ( _text_align_h != RIGHT ) {
-			// calculate comulative_x
-			for ( size_t c = 0; c < _text.size(); c++ )	{
-				if ( _text[c] == 32 ) 
-				{ comulative_x[new_lines] += font-> Get_Heigth ( ) / 2; continue; }
+
+		if ( _text_align_h == ALIGNMENT::RIGHT )
+		{
+			// count new lines
+			for ( size_t c = 0; c < _text.size(); c++ )
+			{
 				if ( _text[c] == '\n' ) 
-				{ new_lines++; comulative_x.push_back ( 0 ); DEBUG ( 4, "Fund new line" ); continue; }
+				{
+					new_lines++;
+					comulative_x.push_back ( 0 );
+					DEBUG ( 4, "Fund new line" );
+					continue;
+				}
+			}	
+		}
+		else
+		{
+			// calculate comulative_x
+			for ( size_t c = 0; c < _text.size(); c++ )
+			{
+				if ( _text[c] == 32 ) 
+				{
+					comulative_x[new_lines] += font-> Get_Heigth ( ) / 2;
+					continue;
+				}
+				if ( _text[c] == '\n' ) 
+				{
+					new_lines++;
+					comulative_x.push_back ( 0 );
+					DEBUG ( 4, "Fund new line" );
+					continue;
+				}
 
 				comulative_x[new_lines] += font-> char_widths[(int)_text[c]];
 			}
@@ -71,18 +84,21 @@ public:
 		DEBUG ( 6, "new linse counterd" );
 		for ( auto i = 0; i <= new_lines; i++ ) {
 			switch ( _text_align_h ) {
-				case CENTER: comulative_x[i] = comulative_x[i] = ( obj->Get_Size ( ).x - comulative_x[i] ) * 0.5; break;
-				case RIGHT: comulative_x[i] = obj->Get_Size ( ).x * 0.5; break;
-				case LEFT: comulative_x[i] = - comulative_x[i] + obj->Get_Size ( ).x * 0.5; break;
+				case ALIGNMENT::CENTER: comulative_x[i] = comulative_x[i] = ( obj->Get_Size ( ).x - comulative_x[i] ) * 0.5; break;
+				case ALIGNMENT::RIGHT: comulative_x[i] = obj->Get_Size ( ).x * 0.5; break;
+				case ALIGNMENT::LEFT: comulative_x[i] = - comulative_x[i] + obj->Get_Size ( ).x * 0.5; break;
+				default: break;
 			}
 		}
+		DEBUG ( 5, "Line advance ", comulative_x );
 		DEBUG ( 6, "alignement setted" );
 		int vertical_m = new_lines * font->Get_Heigth ( );
 		if ( new_lines != 0 ) {
-			switch ( _text_align_h ) {
-				case CENTER: vertical_m *= 0.5; break;
-				case TOP: vertical_m = 0; break;
-				case BOTTOM: break;
+			switch ( _text_align_v ) {
+				case ALIGNMENT::CENTER: vertical_m *= 0.5; break;
+				case ALIGNMENT::TOP: vertical_m = 0; break;
+				case ALIGNMENT::BOTTOM: break;
+				default: break;
 			}
 		}
 		DEBUG ( 6, "vertical alignement setted" );
@@ -115,60 +131,62 @@ public:
 		// emply the buffer
 		if ( glIsBuffer ( _inst ) ) 
 		{ glDeleteBuffers ( 1, &_inst ); GL_CHECK_ERROR; DEBUG (5, "Cleared Buffer"); }
-		
+
 		// load data into buffer
 		instances = _text.size ( );
 		glGenBuffers (1, &_inst); GL_CHECK_ERROR;
-        glBindBuffer(GL_ARRAY_BUFFER, _inst); GL_CHECK_ERROR;
-        glBufferData(GL_ARRAY_BUFFER, (sizeof(int)*4) * instances, instance_buffer, GL_STATIC_DRAW); GL_CHECK_ERROR;
+		glBindBuffer(GL_ARRAY_BUFFER, _inst); GL_CHECK_ERROR;
+		glBufferData(GL_ARRAY_BUFFER, (sizeof(int)*4) * instances, instance_buffer, GL_STATIC_DRAW); GL_CHECK_ERROR;
 		DEBUG (5, "load data into buffer");
 		// setting
 		glBindVertexArray(_quad); GL_CHECK_ERROR;
 		glVertexAttribPointer(1, 4, GL_INT, GL_FALSE, 4 * sizeof(int), (void*)0); GL_CHECK_ERROR;
 		DEBUG (5,"setted vertex array");
 		// unbind
-        glBindVertexArray(0); GL_CHECK_ERROR;
-        glBindBuffer(GL_ARRAY_BUFFER, 0); GL_CHECK_ERROR;
+		glBindVertexArray(0); GL_CHECK_ERROR;
+		glBindBuffer(GL_ARRAY_BUFFER, 0); GL_CHECK_ERROR;
 		DEBUG (4, "Updated Istance Buffer" );
+
+		_is_up_to_date = true;
 	}
 
-    void Start ( ) {
-        DEBUG ( 5, "Staring Text");
-        float vertices[] = { 
-            // pos      // tex
-            0.0f, 1.0f, 0.0f, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 1.0f, 1.0f,
+	void Start ( ) {
+		DEBUG ( 3, "Staring Text");
+		float vertices[] = { 
+			// pos      // tex
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f,
+			1.0f, 0.0f, 1.0f, 1.0f,
 
-            0.0f, 1.0f, 0.0f, 0.0f,
-            1.0f, 0.0f, 1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f, 0.0f
-        };
+			0.0f, 1.0f, 0.0f, 0.0f,
+			1.0f, 0.0f, 1.0f, 1.0f,
+			1.0f, 1.0f, 1.0f, 0.0f
+		};
 
-        glGenVertexArrays(1, &_quad); GL_CHECK_ERROR;
-        glGenBuffers(1, &VBO); GL_CHECK_ERROR;
+		glGenVertexArrays(1, &_quad); GL_CHECK_ERROR;
+		glGenBuffers(1, &VBO); GL_CHECK_ERROR;
 		glGenBuffers(1, &_inst); GL_CHECK_ERROR;
 
 		// load sprite data ( equal per instance )
-        glBindBuffer(GL_ARRAY_BUFFER, VBO); GL_CHECK_ERROR;
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); GL_CHECK_ERROR;
-        glBindBuffer(GL_ARRAY_BUFFER, _inst); GL_CHECK_ERROR;
-        glBufferData(GL_ARRAY_BUFFER, (sizeof(int)*4), calloc (4,sizeof(int)), GL_STATIC_DRAW); GL_CHECK_ERROR;
+		glBindBuffer(GL_ARRAY_BUFFER, VBO); GL_CHECK_ERROR;
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); GL_CHECK_ERROR;
+		glBindBuffer(GL_ARRAY_BUFFER, _inst); GL_CHECK_ERROR;
+		glBufferData(GL_ARRAY_BUFFER, (sizeof(int)*4), calloc (4,sizeof(int)), GL_STATIC_DRAW); GL_CHECK_ERROR;
 		// configuring array buffer
-        glBindVertexArray(_quad); GL_CHECK_ERROR;
+		glBindVertexArray(_quad); GL_CHECK_ERROR;
 		// setting per instance data
-        glEnableVertexAttribArray(0); GL_CHECK_ERROR;
-        glBindBuffer(GL_ARRAY_BUFFER, VBO); GL_CHECK_ERROR;
-        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0); GL_CHECK_ERROR;
+		glEnableVertexAttribArray(0); GL_CHECK_ERROR;
+		glBindBuffer(GL_ARRAY_BUFFER, VBO); GL_CHECK_ERROR;
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0); GL_CHECK_ERROR;
 		// setting instance data
 		glEnableVertexAttribArray(1); GL_CHECK_ERROR;
-        glBindBuffer(GL_ARRAY_BUFFER, _inst); GL_CHECK_ERROR;
+		glBindBuffer(GL_ARRAY_BUFFER, _inst); GL_CHECK_ERROR;
 		glVertexAttribPointer(1, 4, GL_INT, GL_FALSE, 4 * sizeof(int), (void*)0); GL_CHECK_ERROR;
 		// set attribute 1 as an instace array
 		glVertexAttribDivisor(1, 1); GL_CHECK_ERROR;
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0); GL_CHECK_ERROR;
-        glBindVertexArray(0); GL_CHECK_ERROR;
+		glBindBuffer(GL_ARRAY_BUFFER, 0); GL_CHECK_ERROR;
+		glBindVertexArray(0); GL_CHECK_ERROR;
 
 		if ( _shader == "" ) 
 		{ _shader = "text_shader_default"; }
@@ -177,14 +195,16 @@ public:
 		Update_Instance_Buffer ( );
 	}
 
-    void Update ( ) {
+	void Update ( ) {
 		// make sures that the buffer is update once per frame
-		is_up_to_date = false;
-		if ( _new_text != _text ) 
-		{ _text = _new_text; Update_Instance_Buffer ( ); }
+		
+		if ( !_is_up_to_date ) 
+		{
+			Update_Instance_Buffer ( );
+		}
 
 		if ( _text.size() == 0 ) { return; }
-        DEBUG ( 5, "Staring Updating Text");
+
 		// prepare transformations
 		if ( _shader == "" || _font == "" ) { DEBUG ( 1, "Component not set Correctly" ); return; }
 		auto shader = Manager::Get < Shader > ( _shader );
@@ -193,35 +213,28 @@ public:
 		{ shader->setMat4  ( "projection", Camera::UI_Projkection ( ) ); }
 		else
 		{ shader->setMat4  ( "projection", Manager::Camera_Get( _camera )->Projkection ( ) ); }
-		
-		DEBUG ( 6, "Updated Camera uniform");
 
-        shader->setFloat ( "SPRITE_COLUMNS", 16 );
-        shader->setFloat ( "SPRITE_ROWS", 8 );
-        shader->setFloat ( "NUM_OF_SPRITES", (int)(8 * 16) );
-		DEBUG ( 6, "Updated Camera uniform");
+		shader->setFloat ( "SPRITE_COLUMNS", 16 );
+		shader->setFloat ( "SPRITE_ROWS", 8 );
+		shader->setFloat ( "NUM_OF_SPRITES", (int)(8 * 16) );
 
 		mat4 model = obj->Get_Model_Mat ( );
 		shader->setMat4 ( "model", model );
-        DEBUG ( 6, "Updated Model uniform");
 
 		shader->setVec4 ( "spriteColor", _color );
-        DEBUG ( 6, "Updated Frame uniforms");
 
 		Manager::Get < Font > ( _font )->Use();
 
 		glBindVertexArray(_quad); GL_CHECK_ERROR;
-        glDrawArraysInstanced(GL_TRIANGLES, 0, 6, instances); GL_CHECK_ERROR;
-        glBindVertexArray(0); GL_CHECK_ERROR;
-		
-        DEBUG (5, "Drawn Text");
+		glDrawArraysInstanced(GL_TRIANGLES, 0, 6, instances); GL_CHECK_ERROR;
+		glBindVertexArray(0); GL_CHECK_ERROR;
 	}
 
-    void Delete ( ) {
-        glDeleteVertexArrays(1, &_quad);
-        glDeleteBuffers(1, &_inst);
-        glDeleteBuffers(1, &VBO);
-    }
+	void Delete ( ) {
+		glDeleteVertexArrays(1, &_quad);
+		glDeleteBuffers(1, &_inst);
+		glDeleteBuffers(1, &VBO);
+	}
 
 	/* Text * Set ( Font* font, Shader* shader, Camera* camera = nullptr, vec4 color = {1,1,1,1} ) 
 	{ _font = font; _shader = shader; _camera = camera; 
@@ -231,11 +244,12 @@ public:
 	{ _font = font; _shader = shader; _camera = camera; 
 	_color = color; return this; }
 
-	Text * Set ( std::string text, ALIGNMENT al_horizontal, ALIGNMENT al_vertical = CENTER, bool x_warp = false ) {
-		_new_text = text;
+	Text * Set ( std::string text, ALIGNMENT al_horizontal, ALIGNMENT al_vertical = ALIGNMENT::CENTER, bool x_warp = false ) {
+		_text = text;
 		_text_align_h = al_horizontal;
 		_text_align_v = al_vertical;
 		_x_warp = x_warp;
+		_is_up_to_date = false;
 		DEBUG (3, "Text changed: ", text );
 		return this;
 	}
@@ -243,9 +257,13 @@ public:
 	Text * Set ( vec4 color ) 
 	{ _color = color; return this; }
 
-	Text * Set ( std::string text ) { _new_text = text; return this; }
+	Text * Set ( std::string text ) {
+		_text = text;
+		_is_up_to_date = false;
+		return this;
+	}
 
-	std::string * Get ( ) { return &_new_text; }
+	std::string * Get ( ) { return &_text; }
 
 	vec4 * Expose_Color ( ) { return &_color; }
 };
